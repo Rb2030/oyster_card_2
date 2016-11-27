fd7871d0b781ba1c13f637d467d75c1b5a1dc60f
Rb2030 <rossmail2050@gmail.com>
2016-11-27 17:41:46 +0000

    Included Constants

---
+++ lib/oystercard.rb
@@ -0,0 +1,15 @@
+class Oystercard
+
+  attr_reader :balance
+
+  MAXIMUM_BALANCE = 90
+
+  def initialize
+    @balance = 0
+  end
+
+  def top_up(amount)
+    fail "Maximum balance #{MAXIMUM_BALANCE} exceeded" if amount + @balance > MAXIMUM_BALANCE
+    @balance += amount
+  end
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,17 @@
+require "oystercard"
+
 describe Oystercard do
+  it 'has a balance of zero' do
+    expect(subject.balance).to eq(0)
+  end
+
+  it 'can be topped up' do
+    expect{ subject.top_up(5) }.to change{ subject.balance }.by(5)
+  end
+
+  it 'should hava a maximum limit' do
+    maximum_balance = Oystercard::MAXIMUM_BALANCE
+    subject.top_up (maximum_balance)
+    expect{ subject.top_up(1) }.to raise_error "Maximum balance #{maximum_balance} exceeded"
+  end
 end
